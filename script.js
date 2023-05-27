851ad17978561015fcb5f0227b69d8839ec6ea1a
sriharsha8227 <134406141+sriharsha8227@users.noreply.github.com>
2023-05-27 10:45:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿var runBtn = document.getElementById('run-btn');
+runBtn.addEventListener('click', runCode);
+
+function runCode() {
+    var htmlCode = document.getElementById('html-editor').value;
+    var cssCode = document.getElementById('css-editor').value;
+    var jsCode = document.getElementById('js-editor').value;
+
+    var output = document.getElementById('output').contentWindow.document;
+    output.open();
+    output.writeln('<style>' + cssCode + '</style>');
+    output.writeln(htmlCode);
+    output.writeln('<script>' + jsCode + '</script>');
+    output.close();
+}
